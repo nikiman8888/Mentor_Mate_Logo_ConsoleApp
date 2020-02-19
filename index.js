1fd11f20808b52cd4524b44951124f24850998f8
Nikolay <nikiman88@yahoo.com>
2020-02-19 14:46:11 +0200

    Refactoring

--- index.js
+++ index.js
@@ -1,50 +1,13 @@
-const drawer = require('./utils/drawer');
 
-function drawLogo(num) {
+const printingLetter = require('./utils/printingLetter');
 
-    let currentRow = '';
-    const line = '-';
-    const star = '*';
-    const halfRowsLogo = (num + 1) / 2;
+function drawLogo(number) {
 
-    // here we are searching the first(up) part of the logo
-    for (let row = 0; row < halfRowsLogo; row++) {
-        currentRow =
-            drawer(num - row, line) +        // start first M letter
-            drawer(num + row * 2, star) +
-            drawer(num - row * 2, line) +
-            drawer(num + row * 2, star) +
-            drawer(num - row, line) +
-            drawer(num - row, line) +        //start second M letter
-            drawer(num + row * 2, star) +
-            drawer(num - row * 2, line) +
-            drawer(num + row * 2, star) +
-            drawer(num - row, line)
-
-        console.log(currentRow);
-        currentRow = '';
-    }
-    //here we searching the second(down) part of the logo
-    for (let row = 0; row < halfRowsLogo; row++) {
-        currentRow =
-            drawer((num - 1) / 2 - row, line) +     // start first M letter
-            drawer(num, star) +
-            drawer(row * 2 + 1, line) +
-            drawer(((num * 2) - 1) - row * 2, star) +
-            drawer(row * 2 + 1, line) +
-            drawer(num, star) +
-            drawer((num - 1) / 2 - (row), line) +
-            drawer((num - 1) / 2 - row, line) +     //start second M letter
-            drawer(num, star) +
-            drawer(row * 2 + 1, line) +
-            drawer(((num * 2) - 1) - row * 2, star) +
-            drawer(row * 2 + 1, line) +
-            drawer(num, star) +
-            drawer((num - 1) / 2 - (row), line);
-
-        console.log(currentRow);
-        currentRow = '';
-    }
+    printingLetter.printingLetterUpPart(number)
+    printingLetter.printingLetterDownPart(number);
+    printingLetter.printingLetterUpPart(number)
+    printingLetter.printingLetterDownPart(number);
+    
 }
 
 drawLogo(5)
--- utils/printingLetter.js
+++ utils/printingLetter.js
@@ -1,30 +1,37 @@
 const drawer = require('./drawer');
+const line = '-';
+const star = '*';
+let logoRow = '';
 
-function printingLetterUpPart() {
-    let row = ''
+
+function printingLetterUpPart(number) {
+    const halfRowsLogo = (number + 1) / 2;
+    
     for (let row = 0; row < halfRowsLogo; row++) {
-        row =
-            drawer(num - row, line) +
-            drawer(num + row * 2, star) +
-            drawer(num - row * 2, line) +
-            drawer(num + row * 2, star) +
-            drawer(num - row, line)
-        console.log(row);
+        logoRow =
+            drawer(number - row, line) +
+            drawer(number + row * 2, star) +
+            drawer(number - row * 2, line) +
+            drawer(number + row * 2, star) +
+            drawer(number - row, line)
+        console.log(logoRow);
     }
 }
 
-function printingLetterDownPart() {
-    let row = '';
+function printingLetterDownPart(number) {
+    
+    const halfRowsLogo = (number + 1) / 2;
+
     for (let row = 0; row < halfRowsLogo; row++) {
-        row =
-            drawer((num - 1) / 2 - row, line) +    
-            drawer(num, star) +
+        logoRow =
+            drawer((number - 1) / 2 - row, line) +
+            drawer(number, star) +
             drawer(row * 2 + 1, line) +
-            drawer(((num * 2) - 1) - row * 2, star) +
+            drawer(((number * 2) - 1) - row * 2, star) +
             drawer(row * 2 + 1, line) +
-            drawer(num, star) +
-            drawer((num - 1) / 2 - (row), line);
-            console.log(row);
+            drawer(number, star) +
+            drawer((number - 1) / 2 - (row), line);
+        console.log(logoRow);
     }
 }
 
